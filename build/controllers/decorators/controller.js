a12dbc7eba7c71f0c1ceb8ccbca34ad38b67dfb2
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-01-14 20:45:32 -0800

    Use decorator - multiple decorators

--- build/controllers/decorators/controller.js
+++ build/controllers/decorators/controller.js
@@ -1,18 +1,25 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.controller = void 0;
 require("reflect-metadata");
 var AppRouter_1 = require("../../AppRouter");
+var MetadataKeys_1 = require("./MetadataKeys");
 function controller(routePrefix) {
     return function (target) {
         var router = AppRouter_1.AppRouter.getInstance();
         for (var key in target.prototype) {
             // looping for all available methods in Controller
             var routeHandler = target.prototype[key]; // assign method
-            var path = Reflect.getMetadata('path', target.prototype, key); // get metadata
-            var method = Reflect.getMetadata('method', target.prototype, key);
+            var path = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.path, target.prototype, key); // get metadata
+            var method = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.method, target.prototype, key);
+            var middlewares = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.middleware, target, key) || [];
             if (path) {
-                router[method]("" + routePrefix + path, routeHandler);
+                router[method].apply(router, __spreadArray(__spreadArray(["" + routePrefix + path], middlewares), [routeHandler]));
             }
         }
     };
--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
 import { Methods } from './Methods';
+import { MetadataKeys } from './MetadataKeys';
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
@@ -10,15 +11,21 @@ export function controller(routePrefix: string) {
       // looping for all available methods in Controller
       const routeHandler = target.prototype[key]; // assign method
 
-      const path = Reflect.getMetadata('path', target.prototype, key); // get metadata
+      const path = Reflect.getMetadata(
+        MetadataKeys.path,
+        target.prototype,
+        key
+      ); // get metadata
       const method: Methods = Reflect.getMetadata(
-        'method',
+        MetadataKeys.method,
         target.prototype,
         key
       );
+      const middlewares =
+        Reflect.getMetadata(MetadataKeys.middleware, target, key) || [];
 
       if (path) {
-        router[method](`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
       }
     }
   };
